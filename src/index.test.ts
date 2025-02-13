06b935215a608738b899886cffb9d68d47eba6b0
aabccd021 <aabccd021@gmail.com>
2025-02-13 19:27:06 +0700

    chore: add test for empty grant_type in fetch function

--- src/index.test.ts
+++ src/index.test.ts
@@ -367,4 +367,25 @@ describe("fetch https://oauth2.googleapis.com/token", async () => {
       `Invalid grant_type: "refresh_token". Expected "authorization_code".`,
     );
   });
+
+  test("empty grant_type", async () => {
+    const response = await fetch(
+      "https://oauth2.googleapis.com/token",
+      {
+        method: "POST",
+        headers: validHeader,
+        body: new URLSearchParams({
+          code,
+          redirect_uri: "https://example.com/login/callback",
+          code_verifier: codeVerifier,
+        }),
+      },
+      { store: cloneStore(defaultStore) },
+    );
+
+    expect(response.status).toBe(400);
+    expect(response.text()).resolves.toBe(
+      'Invalid grant_type: "null". Expected "authorization_code".',
+    );
+  });
 });
