90b97da9c5e454ade053588bf8903d0f9b236c06
aabccd021 <aabccd021@gmail.com>
2025-02-13 20:23:22 +0700

    Add test for fetching with empty s256 code_verifier

--- src/index.test.ts
+++ src/index.test.ts
@@ -405,6 +405,24 @@ describe("fetch https://oauth2.googleapis.com/token", () => {
     expect(getIdTokenSub(response)).resolves.toBe("kita");
   });
 
+  test("s256 empty code_verifier", async () => {
+    const valid = await validS256();
+    valid.body.delete("code_verifier");
+    const response = await fetch(
+      "https://oauth2.googleapis.com/token",
+      {
+        method: "POST",
+        headers: valid.header,
+        body: valid.body,
+      },
+      { store: valid.store },
+    );
+    expect(response.status).toBe(400);
+    expect(response.text()).resolves.toBe(
+      "Parameter code_verifier is required.",
+    );
+  });
+
   test("empty grant_type", async () => {
     const valid = await getValid(getUrl());
     valid.body.delete("grant_type");
