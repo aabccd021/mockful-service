39de2a350e4dc44f5256a8a5104dd33474abb9c3
aabccd021 <aabccd021@gmail.com>
2025-03-07 07:18:32 +0700

    Remove unnecessary setup code from test operation.

--- test/default.nix
+++ test/default.nix
@@ -27,36 +27,9 @@ let
         pkgs.curl
         server
       ];
-    } ''
-    export NETERO_DIR="./var/lib/netero"
-    mkdir -p "$NETERO_DIR"
+    }
+    (builtins.readFile ./test.sh);
 
-    mkfifo "./ready0.fifo"
-    mkfifo "./ready1.fifo"
-
-    cp -Lr "$INITIAL_DB/db.sqlite" .
-    chmod +w db.sqlite
-
-    server 2>&1 | while IFS= read -r line; do
-      printf '\033[32m[server]\033[0m %s\n' "$line"
-    done &
-
-    auth-mock-server \
-      --db "./db.sqlite" \
-      --on-ready-pipe "./ready1.fifo" \
-      --port 3001 2>&1 | while IFS= read -r line; do
-      printf '\033[34m[accounts.google.com]\033[0m %s\n' "$line"
-    done &
-
-    timeout 5 cat ./ready0.fifo >/dev/null
-    timeout 5 cat ./ready1.fifo >/dev/null
-
-    bash -euo pipefail "$TEST_FILE" 2>&1 | while IFS= read -r line; do
-      printf '\033[33m[client]\033[0m %s\n' "$line"
-    done
-
-    mkdir $out
-  '';
 
   mapTests = prefix: server: dir: names: builtins.listToAttrs (builtins.map
     (name: {
