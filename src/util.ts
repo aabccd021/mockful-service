6e112c259706d1c0daca1ab6e81dd069929cd8d9
aabccd021 <aabccd021@gmail.com>
2025-03-13 07:32:49 +0700

    Update package-lock.json and src/util.ts files for bun and bun-types version 1.2.5.

--- src/util.ts
+++ src/util.ts
@@ -1,24 +1,5 @@
 import type { Database } from "bun:sqlite";
 
-// https://github.com/oven-sh/bun/issues/16062
-// https://github.com/tc39/proposal-arraybuffer-base64
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toBase64#browser_compatibility
-declare global {
-  interface Uint8Array {
-    toBase64(options?: {
-      alphabet?: "base64url" | "base64";
-      omitPadding?: boolean;
-    }): string;
-  }
-
-  interface Uint8ArrayConstructor {
-    fromBase64(
-      base64: string,
-      options?: { alphabet?: "base64url" | "base64" },
-    ): Uint8Array;
-  }
-}
-
 export function errorMessage(...message: string[]): Response {
   console.error(message.join(" "));
   return new Response(message.join(" "), {
