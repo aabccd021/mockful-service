6d51cb0a5b555e68779f288e9a5904ea76f71c05
aabccd021 <aabccd021@gmail.com>
2025-05-26 06:15:15 +0700

    fix: Update paths for TypeScript and Biome commands

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
         cp -Lr ${nodeModules}/node_modules ./node_modules
         cp -Lr ${./src} ./src
         cp -L ${./tsconfig.json} ./tsconfig.json
-        ${lib.getExe pkgs.typescript}
+        ${pkgs.typescript}/bin/tsc
         touch $out
       '';
 
@@ -70,7 +70,7 @@
         cp -L ${./biome.jsonc} ./biome.jsonc
         cp -L ${./tsconfig.json} ./tsconfig.json
         cp -L ${./package.json} ./package.json
-        ${lib.getExe pkgs.biome} check --error-on-warnings
+        ${pkgs.biome}/bin/biome check --error-on-warnings
         touch $out
       '';
 
