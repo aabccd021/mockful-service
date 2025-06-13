a136f42cf85eda04d1294b51f8f2076487b886e0
aabccd021 <aabccd021@gmail.com>
2025-06-13 09:05:26 +0700

    Fix: Update color-formatting in test.sh and replace printf with sed

--- test/test.sh
+++ test/test.sh
@@ -1,25 +1,24 @@
+green=$(printf "\033[32m")
+yellow=$(printf "\033[33m")
+blue=$(printf "\033[34m")
+reset=$(printf "\033[0m")
+
 export NETERO_STATE="./var/lib/netero"
 netero_init
 
 mkfifo "./ready0.fifo"
 mkfifo "./ready1.fifo"
 
-server 2>&1 | while IFS= read -r line; do
-  printf '\033[32m[server]\033[0m %s\n' "$line"
-done &
+server 2>&1 | sed "s/^/${yellow}[server]${reset} /" &
 
 netero-oauth-mock \
   --db "./db.sqlite" \
   --on-ready-pipe "./ready1.fifo" \
-  --port 3001 2>&1 | while IFS= read -r line; do
-  printf '\033[34m[accounts.google.com]\033[0m %s\n' "$line"
-done &
+  --port 3001 2>&1 | sed "s/^/${green}[oauth]${reset} /" &
 
 timeout 5 cat ./ready0.fifo >/dev/null
 timeout 5 cat ./ready1.fifo >/dev/null
 
-bash -euo pipefail "$TEST_FILE" 2>&1 | while IFS= read -r line; do
-  printf '\033[33m[client]\033[0m %s\n' "$line"
-done
+bash -euo pipefail "$TEST_FILE" 2>&1 | sed "s/^/${blue}[test]${reset} /" &
 
 mkdir $out
