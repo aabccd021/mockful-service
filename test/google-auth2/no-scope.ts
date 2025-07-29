9c339a856f0a6f66ad6844d495349524e2d5ba97
aabccd021 <aabccd021@gmail.com>
2025-07-29 16:11:06 +0700

    feat: Add Google authentication test without scope

---
+++ test/google-auth2/no-scope.ts
@@ -0,0 +1,50 @@
+import * as sqlite from "bun:sqlite";
+import { expect } from "bun:test";
+
+const neteroState = process.env["NETERO_STATE"];
+
+new sqlite.Database(`${neteroState}/mock.sqlite`, { strict: true }).exec(`
+  INSERT INTO google_auth_user (sub, email, email_verified) VALUES ('nijika-sub', 'nijika@example.com', 'true');
+  INSERT INTO google_auth_client (id, secret) VALUES ('mock_client_id', 'mock_client_secret');
+`);
+
+// submit login screen
+const loginResponse = await fetch(
+  "http://localhost:3001/https://accounts.google.com/o/oauth2/v2/auth",
+  {
+    method: "POST",
+    redirect: "manual",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      response_type: "code",
+      state: "sfZavFFyK5PDKdkEtHoOZ5GdXZtY1SwCTsHzlh6gHm4",
+      client_id: "mock_client_id",
+      redirect_uri: `https://localhost:3000/login-callback`,
+      user: "nijika-sub",
+    }),
+  },
+);
+
+const location = new URL(loginResponse.headers.get("Location") ?? "");
+const code = location.searchParams.get("code") ?? "";
+
+const tokenResponse = await fetch(
+  "http://localhost:3001/https://oauth2.googleapis.com/token",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${btoa("mock_client_id:mock_client_secret")}`,
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: "https://localhost:3000/login-callback",
+    }),
+  },
+);
+
+expect(tokenResponse.status).toEqual(400);
+expect(await tokenResponse.text()).toEqual("scope is required.");
