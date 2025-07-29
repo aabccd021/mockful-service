3b41c2eae94fc1e45d14c798a0e80efead1c55cd
aabccd021 <aabccd021@gmail.com>
2025-07-29 16:24:49 +0700

    Refactor build script for test cases

--- test/default.nix
+++ test/default.nix
@@ -3,55 +3,45 @@ let
   lib = pkgs.lib;
 
   mkTest =
-    {
-      name,
-      dir,
-      filename,
-    }:
+    dir: filename:
+
     let
+      name = "test-${builtins.baseNameOf dir}-" + (lib.strings.removeSuffix ".ts" filename);
       src = lib.fileset.toSource {
         root = dir;
         fileset = dir + "/${filename}";
       };
+      value = pkgs.runCommand name { } ''
+        green=$(printf "\033[32m")
+        reset=$(printf "\033[0m")
+
+        export NETERO_STATE="./var/lib/netero"
+        ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock-init
+
+        ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock-prepare
+        ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock --port 3001  2>&1 | 
+          sed "s/^/''${green}[mock]''${reset} /" &
+        ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock-wait
+
+        ln -s ${nodeModules}/node_modules ./node_modules
+        cp -L ${src}/${filename} .
+        ${pkgs.bun}/bin/bun test ./${filename}
+
+        mkdir "$out"
+      '';
+
     in
-    pkgs.runCommand name { } ''
-      green=$(printf "\033[32m")
-      reset=$(printf "\033[0m")
-
-      export NETERO_STATE="./var/lib/netero"
-      ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock-init
-
-      ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock-prepare
-      ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock --port 3001  2>&1 | 
-        sed "s/^/''${green}[mock]''${reset} /" &
-      ${pkgs.netero-oauth-mock}/bin/netero-oauth-mock-wait
-
-      ln -s ${nodeModules}/node_modules ./node_modules
-      cp -L ${src}/${filename} .
-      ${pkgs.bun}/bin/bun test ./${filename}
-
-      mkdir "$out"
-    '';
+    {
+      name = name;
+      value = value;
+    };
 
   mapTests =
     dir:
     lib.pipe dir [
       builtins.readDir
       builtins.attrNames
-      (builtins.map (
-        filename:
-        let
-          name = "test-${builtins.baseNameOf dir}-" + (lib.strings.removeSuffix ".ts" filename);
-        in
-        {
-          name = name;
-          value = mkTest {
-            name = name;
-            dir = dir;
-            filename = filename;
-          };
-        }
-      ))
+      (builtins.map (mkTest dir))
       builtins.listToAttrs
     ];
 
