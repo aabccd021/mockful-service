e8e83ed1b355b0c3c24fb371afc5d4ba3dd8306e
aabccd021 <aabccd021@gmail.com>
2025-07-29 17:39:10 +0700

    feat(test): add test for token endpoint

---
+++ test/google-token2/get.ts
@@ -0,0 +1,7 @@
+import { expect } from "bun:test";
+
+const tokenResponse = await fetch(
+  "http://localhost:3001/https://oauth2.googleapis.com/token",
+);
+
+expect(tokenResponse.status).toEqual(405);
