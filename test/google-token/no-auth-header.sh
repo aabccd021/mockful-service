7be4217eec53c7233ef0033a7c18bc2254cce9e5
aabccd021 <aabccd021@gmail.com>
2025-07-29 09:15:54 +0700

    Fix quoting in curl options and data-urlencode for consistency.
    The commit message could be: "Fix quoting and data-urlencode in curl options

--- test/google-token/no-auth-header.sh
+++ test/google-token/no-auth-header.sh
@@ -33,22 +33,20 @@ assert-response-code-equal 200
 
 code=$(cat ./code.txt)
 
-curl_options=" \
-  --cookie '$NETERO_STATE/browser/1/cookie.txt' \
-  --cookie-jar '$NETERO_STATE/browser/1/cookie.txt' \
-  --output '$NETERO_STATE/browser/1/tab/1/body' \
-  --write-out \"%output{$NETERO_STATE/browser/1/tab/1/url.txt}%{url_effective}%output{./header.json}%{header_json}%output{$NETERO_STATE/browser/1/tab/1/response.json}%{json}\" \
+curl \
+  --cookie "$NETERO_STATE/browser/1/cookie.txt" \
+  --cookie-jar "$NETERO_STATE/browser/1/cookie.txt" \
+  --output "$NETERO_STATE/browser/1/tab/1/body" \
+  --write-out "%output{$NETERO_STATE/browser/1/tab/1/response.json}%{json}" \
   --compressed \
   --show-error \
   --silent \
   --location \
   --header 'Content-Type: application/x-www-form-urlencoded' \
   --data-urlencode 'grant_type=authorization_code' \
-  --data-urlencode 'code=$code' \
+  --data-urlencode "code=$code" \
   --data-urlencode 'redirect_uri=http://localhost:3000/login-callback' \
-"
-
-eval "curl $curl_options 'http://localhost:3001/https://oauth2.googleapis.com/token'"
+  'http://localhost:3001/https://oauth2.googleapis.com/token'
 
 assert-response-code-equal 400
 assert-equal 'Authorization header is required.' "$(cat "$NETERO_STATE/browser/1/tab/1/body")"
