c998fbe8813e2aac8cba24db8a958dc0e1f6cd2b
aabccd021 <aabccd021@gmail.com>
2025-07-30 05:41:15 +0700

    Remove deprecated Google OAuth tokens & configurations.

--- test/google-token/unsupported-grant-type.ts
+++ test/google-token/unsupported-grant-type.ts
@@ -1,34 +1,5 @@
-import * as sqlite from "bun:sqlite";
 import { expect } from "bun:test";
 
-const neteroState = process.env["NETERO_STATE"];
-
-new sqlite.Database(`${neteroState}/mock.sqlite`, { strict: true }).exec(`
-  INSERT INTO google_auth_user (sub, email) VALUES ('kita-sub', 'kita@example.com');
-  INSERT INTO google_auth_client (id, secret) VALUES ('mock_client_id', 'mock_client_secret');
-`);
-
-const loginResponse = await fetch(
-  "http://localhost:3001/https://accounts.google.com/o/oauth2/v2/auth",
-  {
-    method: "POST",
-    redirect: "manual",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      scope: "openid",
-      user: "kita-sub",
-      response_type: "code",
-      client_id: "mock_client_id",
-      redirect_uri: `https://localhost:3000/login-callback`,
-      state: "sfZavFFyK5PDKdkEtHoOZ5GdXZtY1SwCTsHzlh6gHm4",
-    }),
-  },
-);
-
-const location = new URL(loginResponse.headers.get("Location") ?? "");
-const code = location.searchParams.get("code") ?? "";
 const tokenResponse = await fetch("http://localhost:3001/https://oauth2.googleapis.com/token", {
   method: "POST",
   headers: {
@@ -37,7 +8,6 @@ const tokenResponse = await fetch("http://localhost:3001/https://oauth2.googleap
   },
   body: new URLSearchParams({
     grant_type: "foo",
-    code,
     redirect_uri: "https://localhost:3000/login-callback",
   }),
 });
