cd5b24282f75d0d8e10f2aa4137d87bb82bc00a4
aabccd021 <aabccd021@gmail.com>
2025-07-29 17:44:51 +0700

    feat: Add test for Google OAuth token retrieval

---
+++ test/google-token2/auth-not-basic.ts
@@ -0,0 +1,54 @@
+import * as sqlite from "bun:sqlite";
+import { expect } from "bun:test";
+
+const neteroState = process.env["NETERO_STATE"];
+
+new sqlite.Database(`${neteroState}/mock.sqlite`, { strict: true }).exec(`
+  INSERT INTO google_auth_user (sub, email) VALUES ('kita-sub', 'kita@example.com');
+  INSERT INTO google_auth_client (id, secret) VALUES ('mock_client_id', 'mock_client_secret');
+`);
+
+const loginResponse = await fetch(
+  "http://localhost:3001/https://accounts.google.com/o/oauth2/v2/auth",
+  {
+    method: "POST",
+    redirect: "manual",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      scope: "openid",
+      user: "kita-sub",
+      response_type: "code",
+      client_id: "mock_client_id",
+      redirect_uri: `https://localhost:3000/login-callback`,
+      state: "sfZavFFyK5PDKdkEtHoOZ5GdXZtY1SwCTsHzlh6gHm4",
+    }),
+  },
+);
+
+const location = new URL(loginResponse.headers.get("Location") ?? "");
+const code = location.searchParams.get("code") ?? "";
+
+const tokenResponse = await fetch(
+  "http://localhost:3001/https://oauth2.googleapis.com/token",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Advanced ${btoa("mock_client_id:mock_client_secret")}`,
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: "https://localhost:3000/login-callback",
+    }),
+  },
+);
+
+expect(tokenResponse.status).toEqual(400);
+
+expect(tokenResponse.json()).resolves.toEqual({
+  error: "invalid_request",
+  error_description: "Could not determine client ID from request.",
+});
