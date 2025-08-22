2a3a4b37d248cd88f651500046bf5863d73885f0
aabccd021 <aabccd021@gmail.com>
2025-08-23 06:05:54 +0700

    remove unnecessary route

--- src/accounts.google.com/route.ts
+++ src/accounts.google.com/route.ts
@@ -1,16 +1,11 @@
 import type { Context } from "@util";
 import * as o from "./o/route.ts";
-import * as signin from "./signin/route.ts";
 
 export async function handle(ctx: Context, paths: string[]): Promise<Response> {
   const [path, ...subPaths] = paths;
 
   if (path === "o") {
     return o.handle(ctx, subPaths);
-  }
-
-  if (path === "signin") {
-    return signin.handle(ctx, subPaths);
   }
 
   return new Response("Not Found", { status: 404 });
