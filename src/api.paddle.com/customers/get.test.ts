e4b4b3fab2d1355366b1a2148b309e87f3ea0193
aabccd021 <aabccd021@gmail.com>
2025-09-12 11:24:26 +0700

    Implement api key permissions and customer tests.

--- src/api.paddle.com/customers/get.test.ts
+++ src/api.paddle.com/customers/get.test.ts
@@ -224,3 +224,129 @@ using ctx = test.init();
   if (customers[0]?.marketing_consent !== false) throw new Error();
   if (customers[0]?.locale !== "en") throw new Error();
 }
+
+// kita_email => kita
+// yamada_email => yamada
+// kita_email + yamada_email => kita,yamada
+// kita_id => kita
+// kita_id & kita_email => kita
+// kita_id & yamada_email =>
+// kita_id & kita_email + yamada_email => kita
+// yamada_id => yamada_email
+// yamada_id & kita_email =>
+// yamada_id & yamada_email => yamada
+// yamada_id & kita_email + yamada_email => yamada
+// kita_id + yamada_id => kita,yamada
+// kita_id + yamada_id & kita_email => kita
+// kita_id + yamada_id & yamada_email => yamada
+// kita_id + yamada_id & kita_email + yamada_email => kita,yamada
+
+const parameters = [
+  { email: ["kita"], result: ["kita"] },
+  { email: ["yamada"], result: ["yamada"] },
+  { email: ["kita", "yamada"], result: ["kita", "yamada"] },
+  { id: ["kita"], result: ["kita"] },
+  { id: ["kita"], email: ["kita"], result: ["kita"] },
+  { id: ["kita"], email: ["yamada"], result: [] },
+  { id: ["kita"], email: ["kita", "yamada"], result: ["kita"] },
+  { id: ["yamada"], result: ["yamada"] },
+  { id: ["yamada"], email: ["kita"], result: [] },
+  { id: ["yamada"], email: ["yamada"], result: ["yamada"] },
+  { id: ["yamada"], email: ["kita", "yamada"], result: ["yamada"] },
+  { id: ["kita", "yamada"], result: ["kita", "yamada"] },
+  { id: ["kita", "yamada"], email: ["kita"], result: ["kita"] },
+  { id: ["kita", "yamada"], email: ["yamada"], result: ["yamada"] },
+  { id: ["kita", "yamada"], email: ["kita", "yamada"], result: ["kita", "yamada"] },
+  { result: ["kita", "nijika", "yamada"] },
+];
+
+for (const param of parameters) {
+  console.info(`parameterized: ${JSON.stringify(param)}`);
+
+  test.resetDb(ctx);
+
+  new sqlite.Database(ctx.dbPath).exec(`
+    INSERT INTO paddle_account (id) VALUES ('mock_account_id');
+    INSERT INTO paddle_api_key (account_id, key) VALUES (
+      'mock_account_id', 
+      'pdl_live_apikey_01gtgztp8f4kek3yd4g1wrksa3_q6TGTJyvoIz7LDtXT65bX7_AQO'
+    );
+  `);
+
+  const nijika = await fetch("http://localhost:3001/https://sandbox-api.paddle.com/customers", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer pdl_live_apikey_01gtgztp8f4kek3yd4g1wrksa3_q6TGTJyvoIz7LDtXT65bX7_AQO",
+    },
+    body: JSON.stringify({
+      email: "nijika@example.com",
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => res.data);
+
+  const kita = await fetch("http://localhost:3001/https://sandbox-api.paddle.com/customers", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer pdl_live_apikey_01gtgztp8f4kek3yd4g1wrksa3_q6TGTJyvoIz7LDtXT65bX7_AQO",
+    },
+    body: JSON.stringify({
+      email: "kita@example.com",
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => res.data);
+
+  const yamada = await fetch("http://localhost:3001/https://sandbox-api.paddle.com/customers", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer pdl_live_apikey_01gtgztp8f4kek3yd4g1wrksa3_q6TGTJyvoIz7LDtXT65bX7_AQO",
+    },
+    body: JSON.stringify({
+      email: "yamada@example.com",
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => res.data);
+
+  const customers = { nijika, kita, yamada };
+
+  const listUrl = new URL("http://localhost:3001/https://sandbox-api.paddle.com/customers");
+
+  if (param.email !== undefined) {
+    const emailParams = Object.entries(customers)
+      .filter(([key]) => param.email.includes(key))
+      .map(([, customer]) => customer.email);
+    listUrl.searchParams.set("email", emailParams.join(","));
+  }
+
+  if (param.id !== undefined) {
+    const idParams = Object.entries(customers)
+      .filter(([key]) => param.id.includes(key))
+      .map(([, customer]) => customer.id);
+    listUrl.searchParams.set("id", idParams.join(","));
+  }
+
+  const response = await fetch(listUrl, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer pdl_live_apikey_01gtgztp8f4kek3yd4g1wrksa3_q6TGTJyvoIz7LDtXT65bX7_AQO",
+    },
+  });
+
+  if (response.status !== 200) throw new Error();
+  const responseBody = await response.json();
+  const responseEmails = responseBody.data.map((c: { email: string }) => c.email).toSorted();
+
+  const expectedEmails = Object.entries(customers)
+    .filter(([key]) => param.result.includes(key))
+    .map(([, customer]) => customer.email)
+    .toSorted();
+
+  for (const [emailIdx, email] of expectedEmails.entries()) {
+    if (responseEmails[emailIdx] !== email) {
+      console.error({ expectedEmails, responseEmails });
+      throw new Error();
+    }
+  }
+}
